e456b078ffeb431f3bd2cddf7be81e424b77a7dd
isheraz <sherazahmed93@gmail.com>
2017-02-19 15:32:12 +0500

    favicon added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
     /* Setting Border to height of page */
-    var height = $(this).height() - 60;
+    var height = $(this).height();
     $("#km-wrapper").css("height", height);
 });
