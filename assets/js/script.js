bd29c58b17885cea1a1b9eb808f4f5b31786e3ec
Sheraz Ahmed <sherazahmed93@gmail.com>
2017-02-24 18:55:29 +0500

    Removed js height

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-    /* Setting Border to height of page */
-    var height = $(this).height();
-    $("#km-wrapper").css("height", height);
+    // /* Setting Border to height of page */
+    // var height = $(this).height();
+    // $("#km-wrapper").css("height", height);
 });
